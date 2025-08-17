b520a5be6d68bc63e54cb3e332eb1b24f894482c
ArthurHeitmann <37270165+ArthurHeitmann@users.noreply.github.com>
2025-08-17 10:56:52 +0200

    check browser compatibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,69 +1,18 @@
-
 <script lang="ts">
 	import { browser } from "$app/environment";
-	import OverlayLayer from "$lib/components/OverlayLayer/OverlayLayer.svelte";
-	import PagesBar from "$lib/components/PagesBar/PagesBar.svelte";
-	import PageView from "$lib/components/PageView/PageView.svelte";
-	import { AppState } from "$lib/datamodel/AppState.svelte";
-	import { StorageKeys } from "$lib/datamodel/constants";
-	import { darkTheme, globals } from "$lib/datamodel/globals.svelte";
-	import { starterSaveJson } from "$lib/datamodel/starterSave";
-	import { EventStream } from "$lib/EventStream.svelte";
-	import { loadFormLocalStorage } from "$lib/localStorageState.svelte";
-	import { onMount, setContext } from "svelte";
+    import AppStateView from "./AppStateView.svelte";
 
-	onMount(() => {
-		const applyTheme = (isDark: boolean) => {
-			if (isDark) {
-				document.documentElement.classList.add("dark-theme");
-				document.documentElement.classList.remove("light-theme");
-			} else {
-				document.documentElement.classList.add("light-theme");
-				document.documentElement.classList.remove("dark-theme");
-			}
-		};
-		applyTheme($darkTheme);
-		darkTheme.subscribe(applyTheme);
-	});
+	const isBrowserSupported = (() => {
+		if (!browser) {
+			return true;
+		}
+		const set = new Set();
+		const setValues = set.values();
+		const supportsSetIterMap = typeof setValues?.map === "function";
 
-	const app = (() => {
-		try {
-			const savedJson = loadFormLocalStorage(StorageKeys.appState, null);
-			if (savedJson !== null) {
-				return AppState.fromJSON(savedJson);
-			}
-		} catch (e) {
-			console.error("Failed to load app state from local storage:", e);
-		}
-		if (browser) {
-			const starterSave = JSON.parse(starterSaveJson);
-			return AppState.fromJSON(starterSave);
-		} else {
-			return AppState.newDefault();
-		}
+		return supportsSetIterMap;
 	})();
 
-	const eventStream = new EventStream();
-	setContext("overlay-layer-event-stream", eventStream);
-
-	function onMouseEvent(event: MouseEvent) {
-		globals.mousePosition.x = event.clientX;
-		globals.mousePosition.y = event.clientY;
-	}
-	function onTouchEvent(event: TouchEvent) {
-		if (event.touches.length > 0) {
-			globals.mousePosition.x = event.touches[0].clientX;
-			globals.mousePosition.y = event.touches[0].clientY;
-		}
-	}
-	
-	setContext("app-state", app);
-
-
-	function onBeforeUnload() {
-		const json = app.toJSON({ forceToJson: true });
-		app.saveToLocalStorage(json);
-	}
 </script>
 
 <svelte:head>
@@ -71,38 +20,27 @@
 	<meta name="description" content="A tool for planning, managing and visualizing Satisfactory factories." />
 </svelte:head>
 
-<!-- maybe non passive touch handlers + prevent default -->
-<svelte:window
-	onbeforeunload={onBeforeUnload}
-	onmousedown={onMouseEvent}
-	onmousemove={onMouseEvent}
-	ontouchstart={onTouchEvent}
-	ontouchmove={onTouchEvent}
-/>
-
-<OverlayLayer eventStream={eventStream}>
-	<div class="home">
-		<div class="page-view">
-			{#key app.currentPage.id}
-				<PageView page={app.currentPage} />
-			{/key}
-		</div>
-		<PagesBar appState={app} />
+{#if isBrowserSupported}
+	<AppStateView />
+{:else}
+	<div class="unsupported-browser">
+		<p>
+			This browser is not supported. Please update to a newer version.
+		</p>
 	</div>
-</OverlayLayer>
+{/if}
 
 <style lang="scss">
-	.home {
-		display: flex;
-		flex-direction: column;
+	.unsupported-browser {
 		width: 100%;
 		height: 100dvh;
-	}
-
-	.page-view {
-		flex: 1;
 		display: flex;
-		justify-content: center;
 		align-items: center;
+		justify-content: center;
+		font-size: 2rem;
+		font-weight: bold;
+		text-align: center;
+		background-color: black;
+		color: rgb(224, 17, 17);
 	}
 </style>
---
+++ src/routes/AppStateView.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+	import OverlayLayer from "$lib/components/OverlayLayer/OverlayLayer.svelte";
+	import PagesBar from "$lib/components/PagesBar/PagesBar.svelte";
+	import PageView from "$lib/components/PageView/PageView.svelte";
+	import { AppState } from "$lib/datamodel/AppState.svelte";
+	import { StorageKeys } from "$lib/datamodel/constants";
+	import { darkTheme, globals } from "$lib/datamodel/globals.svelte";
+	import { starterSaveJson } from "$lib/datamodel/starterSave";
+	import { EventStream } from "$lib/EventStream.svelte";
+	import { loadFormLocalStorage } from "$lib/localStorageState.svelte";
+	import { onMount, setContext } from "svelte";
+
+	onMount(() => {
+		const applyTheme = (isDark: boolean) => {
+			if (isDark) {
+				document.documentElement.classList.add("dark-theme");
+				document.documentElement.classList.remove("light-theme");
+			} else {
+				document.documentElement.classList.add("light-theme");
+				document.documentElement.classList.remove("dark-theme");
+			}
+		};
+		applyTheme($darkTheme);
+		darkTheme.subscribe(applyTheme);
+	});
+
+	const app = (() => {
+		try {
+			const savedJson = loadFormLocalStorage(StorageKeys.appState, null);
+			if (savedJson !== null) {
+				return AppState.fromJSON(savedJson);
+			}
+		} catch (e) {
+			console.error("Failed to load app state from local storage:", e);
+		}
+		if (browser) {
+			const starterSave = JSON.parse(starterSaveJson);
+			return AppState.fromJSON(starterSave);
+		} else {
+			return AppState.newDefault();
+		}
+	})();
+
+	const eventStream = new EventStream();
+	setContext("overlay-layer-event-stream", eventStream);
+
+	function onMouseEvent(event: MouseEvent) {
+		globals.mousePosition.x = event.clientX;
+		globals.mousePosition.y = event.clientY;
+	}
+	function onTouchEvent(event: TouchEvent) {
+		if (event.touches.length > 0) {
+			globals.mousePosition.x = event.touches[0].clientX;
+			globals.mousePosition.y = event.touches[0].clientY;
+		}
+	}
+	
+	setContext("app-state", app);
+
+
+	function onBeforeUnload() {
+		const json = app.toJSON({ forceToJson: true });
+		app.saveToLocalStorage(json);
+	}
+</script>
+
+<!-- maybe non passive touch handlers + prevent default -->
+<svelte:window
+	onbeforeunload={onBeforeUnload}
+	onmousedown={onMouseEvent}
+	onmousemove={onMouseEvent}
+	ontouchstart={onTouchEvent}
+	ontouchmove={onTouchEvent}
+/>
+
+<OverlayLayer eventStream={eventStream}>
+	<div class="home">
+		<div class="page-view">
+			{#key app.currentPage.id}
+				<PageView page={app.currentPage} />
+			{/key}
+		</div>
+		<PagesBar appState={app} />
+	</div>
+</OverlayLayer>
+
+<style lang="scss">
+	.home {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100dvh;
+	}
+
+	.page-view {
+		flex: 1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
